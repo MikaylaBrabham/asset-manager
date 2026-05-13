c4ece38e1e1ae1ec0edb7c3174be4861a880d427
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-13 01:16:47 -0500

    added house and vehicle classes

--- src/main/java/com/pluralsight/Assets.java
+++ src/main/java/com/pluralsight/Assets.java
@@ -1,4 +1,51 @@
 package com.pluralsight;
 
 public class Assets {
+
+    //add given properties
+    private String description;
+    private String dateAcquired;
+    private double originalCost;
+
+    //add constructor
+
+    public Assets(String description, String dateAcquired, double originalCost) {
+        this.description = description;
+        this.dateAcquired = dateAcquired;
+        this.originalCost = originalCost;
+    }
+
+    //add getters & setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateAcquired() {
+        return dateAcquired;
+    }
+
+    public void setDateAcquired(String dateAcquired) {
+        this.dateAcquired = dateAcquired;
+    }
+
+    public double getOriginalCost() {
+        return originalCost;
+    }
+
+    public void setOriginalCost(double originalCost) {
+        this.originalCost = originalCost;
+    }
+
+    //add get value derived method
+
+    private double getValue(){
+        return this.originalCost;
+    }
+
+
 }
+
